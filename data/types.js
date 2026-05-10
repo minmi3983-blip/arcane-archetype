d470e3e1efd3b0ef2d3c2405b4b1a77c62ce9797
minmi3983-blip <minmi3983@gmail.com>
2026-05-10 19:11:28 +0900

    Update types.js

--- data/types.js
+++ data/types.js
@@ -5,6 +5,7 @@ window.types = {
     y: 1,
     color: "#ff4d4d",
     catchcopy: "人を巻き込み、場を灯す魔法使い",
+    image: "assets/images/types/arile/arile_portrait.png",
     description:
       "あなたは交流と発信を楽しむタイプ。企画や会話の中心になりやすく、場を動かす力を持っています。"
   },
@@ -15,6 +16,7 @@ window.types = {
     y: -1,
     color: "#9c8aec",
     catchcopy: "自分の世界を紡ぐ魔法使い",
+    image: "assets/images/types/nimia/nimia_portrait.png",
     description:
       "あなたは自分の創作世界を深く掘り下げるタイプ。静かに強いこだわりを持っています。"
   },
@@ -29,12 +31,13 @@ window.types = {
       "あなたは人の話を聞き、交流を支えるタイプ。安心感を与える存在です。"
   },
 
-  garp: {
+  galp: {
     name: "ガルプちゃんタイプ",
     x: -1,
     y: -1,
     color: "#e3f57e",
     catchcopy: "静かに世界を見つめる魔法使い",
+    image: "assets/images/types/galp/galp_portrait.png",
     description:
       "あなたは自分のペースを大切にするタイプ。深く作品を味わい、静かに創作と向き合います。"
   },
@@ -45,6 +48,7 @@ window.types = {
     y: 0,
     color: "#f7b6ee",
     catchcopy: "どんな世界にも馴染める自由な魔法使い",
+    image: "assets/images/types/chloe/chloe_portrait.png"
     description:
       "あなたは場面によって柔軟に立ち回れるバランス型。様々な人と自然に関われます。"
   }
